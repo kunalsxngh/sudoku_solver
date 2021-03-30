555b5dd938ffcccb43a2cc7d395cf1bbbd1bf437
kunalsxngh <ksingh143@hotmail.com>
2021-03-30 11:37:36 +0100

    Creating the print board and find empty functions

---
+++ working.py
@@ -0,0 +1,34 @@
+board = [
+    [7,8,0,4,0,0,1,2,0],
+    [6,0,0,0,7,5,0,0,9],
+    [0,0,0,6,0,1,0,7,8],
+    [0,0,7,0,4,0,2,6,0],
+    [0,0,1,0,5,0,9,3,0],
+    [9,0,4,0,6,0,0,0,5],
+    [0,7,0,3,0,0,0,1,2],
+    [1,2,0,0,0,7,4,0,0],
+    [0,4,9,2,0,6,0,0,7]
+]
+
+def print_board(board):
+    for i in range(len(board)):
+        if i % 3 == 0 and i != 0: #After ever 3 rows print this line
+            print("- - - - - - - - - - - - -")
+        
+        for j in range(len(board[0])):
+            if j % 3 == 0 and j != 0: # Print a line to seperate the grids line
+                print (" |  ", end="") # End is included so that it does not print \n (a new line)
+
+            if j == 8:
+                print(board[i][j]) # Without end so that it prints a new line
+
+            else:
+                print(str(board[i][j]) + " ", end="")
+
+def find_empty(board):
+    for i in range(len(board)):
+        for j in range(len(board[0])):
+            if board[i][j] == 0:
+                return (i, j) # row, column
+
+
