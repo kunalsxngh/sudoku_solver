c9a79c07eaae6be904ab87d72d373c2d0e1d5a04
kunalsxngh <ksingh143@hotmail.com>
2021-03-30 13:10:21 +0100

    Adding the solving function, which includes the valid function

--- working.py
+++ working.py
@@ -1,4 +1,4 @@
-board = [
+bo = [
     [7,8,0,4,0,0,1,2,0],
     [6,0,0,0,7,5,0,0,9],
     [0,0,0,6,0,1,0,7,8],
@@ -30,5 +30,47 @@ def find_empty(board):
         for j in range(len(board[0])):
             if board[i][j] == 0:
                 return (i, j) # row, column
+    return None
 
 
+def valid(board, num, pos):
+
+    # Check row
+    for i in range(len(board[0])):
+        if board[pos[0]][i] == num and pos[1] != i:
+            return False
+
+    # Check Column
+    for i in range(len(board[0])):
+        if board[i][pos[1]] == num and pos[0] != i:
+            return False
+    
+    # Check grid
+    grid_x = pos[1] // 3
+    grid_y = pos[0] // 3
+
+    for i in range(grid_y * 3, grid_y * 3 + 3):
+        for j in range(grid_x * 3, grid_x * 3 + 3):
+            if board[i][j] == num and (i, j) != pos:
+                return False
+
+    return True
+
+
+def solve(board):
+    find = find_empty(board)
+    if not find:
+        return True
+    else:
+        row, col = find
+    
+    for i in range(1, 10):
+        if valid(board, i, (row, col)):
+            board[row][col] = i
+
+            if solve(board):
+                return True
+            
+            board[row][col] = 0
+
+    return False
